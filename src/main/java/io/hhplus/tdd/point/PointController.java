57991d5b33487f01109dbb28817cb387aff4146a
corncode8 <myeongcheol8@naver.com>
2024-03-28 23:17:31 +0900

    Controller 구현

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -18,36 +18,32 @@ public class PointController {
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
-//    @GetMapping("{id}")
-//    public UserPoint point(@PathVariable("id") Long id) throws InterruptedException{
-//
-//
-//    }
+    @GetMapping("{id}")
+    public UserPoint point(@PathVariable("id") Long id) throws InterruptedException{
+        return userPointService.read(id);
+    }
 
     /**
      * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
      */
-//    @GetMapping("{id}/histories")
-//    public List<PointHistory> history(@PathVariable("id") Long id) {
-//
-//
-//    }
+    @GetMapping("{id}/histories")
+    public List<PointHistory> history(@PathVariable("id") Long id) {
+        return userPointService.readList(id);
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
      */
-//    @PatchMapping("{id}/charge")
-//    public UserPoint charge(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
-//
-
-//    }
+    @PatchMapping("{id}/charge")
+    public UserPoint charge(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
+        return userPointService.charge(id, amount);
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
-//    @PatchMapping("{id}/use")
-//    public UserPoint use(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
-//
-
-//    }
+    @PatchMapping("{id}/use")
+    public UserPoint use(@PathVariable Long id, @RequestBody Long amount) throws InterruptedException{
+        return userPointService.use(id, amount);
+    }
 }
